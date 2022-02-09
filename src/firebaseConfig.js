29448159a5a64ab1962aa279f4dea1fbf831bf41
alotofeve <Duo.xu74@gmail.com>
2022-02-09 10:38:16 -0800

    the first firebase exercise

---
+++ src/firebaseConfig.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+/*
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDj2Td3mtu_A4t9dVAreKL7YRKQ6RRKUNU",
+  authDomain: "react-auth-4b781.firebaseapp.com",
+  projectId: "react-auth-4b781",
+  storageBucket: "react-auth-4b781.appspot.com",
+  messagingSenderId: "203778071515",
+  appId: "1:203778071515:web:86a61863d588ec3e3da5fe",
+  measurementId: "G-X444842VS9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+*/
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  apiKey: "AIzaSyDj2Td3mtu_A4t9dVAreKL7YRKQ6RRKUNU",
+  authDomain: "react-auth-4b781.firebaseapp.com",
+  databaseURL: "https:",
+  projectId: "react-auth-4b781",
+  storageBucket: "react-auth-4b781.appspot.com",
+  messagingSenderId: "203778071515",
+  appId: "1:203778071515:web:86a61863d588ec3e3da5fe",
+  measurementId: "G-X444842VS9"
+};
